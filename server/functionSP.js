868c5ed3d418cc86a4175b7ecd30c6cdca84865d
dinhvandai63 <dvdai63@gmail.com>
2020-06-07 14:39:06 +0700

    add file server

---
+++ server/functionSP.js
@@ -0,0 +1,9 @@
+// You can require any js file, you just need to declare what you want to expose.
+    module.exports = {
+        add: function (a, b) {
+            return a+b;
+        },
+        bar: function () {
+            // whatever
+        }
+    };
